c5a1fdd504c88c1b95c6681dd52cc64e64f8436a
Abhishek Pal <a.pal@planetsmartcity.com>
2025-11-25 20:25:25 +0530

    `Added import of path and fileURLToPath from url module`

--- backend/src/schema.js
+++ backend/src/schema.js
@@ -1,17 +1,30 @@
 import { loadFiles } from "@graphql-tools/load-files";
 import { mergeTypeDefs, mergeResolvers } from "@graphql-tools/merge";
 import { makeExecutableSchema } from "@graphql-tools/schema";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const projectRoot = process.cwd();
 
 export default async function buildSchema() {
-  const typeDefs = await loadFiles("src/**/*.graphql", {
+  const typeDefs = await loadFiles(path.join(projectRoot, "src/**/*.graphql"), {
     useRequire: false,
     ignoreIndex: true,
   });
 
-  const resolvers = await loadFiles("src/**/*.resolvers.js", {
-    useRequire: false,
-    ignoreIndex: true,
-  });
+  const resolvers = await loadFiles(
+    path.join(projectRoot, "src/**/*.resolvers.js"),
+    {
+      useRequire: false,
+      ignoreIndex: true,
+    }
+  );
+
+  if (typeDefs.length === 0) {
+  }
 
   return makeExecutableSchema({
     typeDefs: mergeTypeDefs(typeDefs),
